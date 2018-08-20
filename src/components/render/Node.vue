586aaaed1acfc99bd4e1a375d748d50466f20701
bestvist <bestvist@qq.com>
2018-08-21 00:11:20 +0800

    init

---
+++ src/components/render/Node.vue
@@ -0,0 +1,55 @@
+<!--<template>-->
+    <!--<span>-->
+    <!--<span v-for="(node,index) in nodes" :key="index">-->
+    <!--<div v-if="node.label==='Box'" :style="computeStyle(node.id)">-->
+        <!--<Node v-if="node.children&&node.children.length"-->
+              <!--v-for="child in node.children" :key="child.id" :node="child"></Node>-->
+    <!--</div>-->
+    <!--<button v-if="node.label==='Button'" :style="computeStyle(node.id)">-->
+    <!--<Node v-if="node.children&&node.children.length" :nodes="node.children"></Node>-->
+    <!--</button>-->
+    <!--<img v-if="node.label==='Img'" :style="computeStyle(node.id)"/>-->
+    <!--<span v-if="node.label==='Text'" :style="computeStyle(node.id)"></span>-->
+    <!--</span>-->
+    <!--</span>-->
+<!--</template>-->
+
+<script>
+    import Node from '@/components/render/Node'
+
+    export default {
+        name: 'Node',
+        components: {
+            Node
+        },
+        render(h){
+           return h(this.node.label==='Box'? 'Div':'Span')
+        },
+        props: {
+            node: {
+                type: Object,
+                default: () => {
+                    return {}
+                }
+            },
+            attrs: {
+                type: Array
+            }
+        },
+        data() {
+            return {}
+        },
+        methods: {
+            computeStyle(id) {
+                let style = {};
+                this.attrs.some(item => {
+                    if (item.id === id) {
+                        style = item.styles;
+                    }
+                    return item.id === id;
+                });
+                return style;
+            }
+        }
+    }
+</script>
--- src/components/render/index.vue
+++ src/components/render/index.vue
@@ -1,61 +1,49 @@
 <template>
     <div class="content-render">
-        <template v-for="(ele,index) in data">
-            <div :key="index" v-if="ele.label==='Box'" :style="computeStyle(ele.id)"></div>
-            <button :key="index" v-if="ele.label==='Button'" :style="computeStyle(ele.id)"></button>
-            <img :key="index" v-if="ele.label==='Img'" :style="computeStyle(ele.id)" />
-            <span :key="index" v-if="ele.label==='Text'" :style="computeStyle(ele.id)"></span>
-            <!--<Render :key="index" v-if="ele.children.length" />-->
-        </template>
-        <Render/>
+        <Node v-for="node in data" :key="node.id" :node="node" :attrs="attrs"></Node>
     </div>
 </template>
 
 <script>
-//import Render from "@/components/render";
-export default {
-    name: "Render",
-    components: {},
-    props: {
-        data: {
-            type: Array,
-            default: () => {
-                return [];
+    import Node from "@/components/render/Node";
+
+    export default {
+        name: "Render",
+        components: {
+            Node
+        },
+        props: {
+            data: {
+                type: Array,
+                default: () => {
+                    return [];
+                }
+            },
+            attrs: {
+                type: Array,
+                default: () => {
+                    return [];
+                }
             }
         },
-        attrs: {
-            type: Array,
-            default: () => {
-                return [];
+        watch: {
+            data() {
+                console.log(this.data);
+                console.log(this.attrs);
             }
-        }
-    },
-    data() {
-        return {};
-    },
-    watch: {
+        },
         data() {
-            console.log(this.data);
+            return {};
+        },
+        methods: {},
+        mounted() {
+
         }
-    },
-    methods: {
-        computeStyle(id) {
-            let style = {};
-            this.attrs.some(item => {
-                if (item.id == id) {
-                    style = item.styles;
-                }
-                return item.id == id;
-            });
-            return style;
-        }
-    },
-    mounted() {}
-};
+    };
 </script>
 
 <style lang="scss" scoped>
-.content-render {
-}
+    .content-render {
+    }
 </style>
 
